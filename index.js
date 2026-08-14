b317807bab6f642180854ca106443ef858da1664
Akusolomon <47886102+Akusolomon@users.noreply.github.com>
2026-08-14 16:01:48 +0300

    console

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const uri = process.env.MONGODB_URI;
 require('dotenv').config();
-
+console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
+console.log("Bot token exists:", !!process.env.BOT_TOKEN);
 
 
 
